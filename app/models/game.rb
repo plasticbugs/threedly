8f7b1982e4b6079e30ecef7e405b8209f4a472a1
Scott Moschella <scottmosch@gmail.com>
2010-10-24 23:32:41 -0700

    Games have Attachments

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ActiveRecord::Base
   has_many :quotes, :as => :quotable
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image
 end
